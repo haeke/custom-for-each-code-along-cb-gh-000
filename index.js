4805e18ba66d6bbfbd04e55ec97ab8b0271d620a
edwin rivera <waehake@gmail.com>
2018-01-22 01:03:10 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+
+var dogs = [
+  {
+    name: 'fido', age: 3,
+  },
+  {
+    name: 'odie', age: 5,
+  },
+  {
+    name: 'ralph', age: 7,
+  }
+];
+
+for ( let i = 0; i < dogs.length; i++) {
+  console.log(i);
+}
